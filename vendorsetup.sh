fd51dda9596e2933b743c4bb76f49daa40a77f29
Tejaskamat <kamattejas23@gmail.com>
2023-01-01 12:08:37 +0700

    mojito: Add vendorsetup.sh
    Signed-off-by: Tejaskamat <kamattejas23@gmail.com>
    
    mojito: vendorsetup: add clang 16.1
    
    Signed-off-by: Tejaskamat <kamattejas23@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# Paths
+dt="device/xiaomi/mojito"
+vt="vendor/xiaomi/mojito"
+kt="kernel/xiaomi/mojito"
+clang="prebuilts/clang/host/linux-x86/clang-9342159"
+
+# Clone device dependecies
+clone(){
+	git clone https://gitlab.com/spiceos/android_prebuilts_clang_host_linux-x86_clang-9342159 --depth=1 $clang
+	
+	if [ "depth" -eq 69 ]
+	then
+		git clone https://github.com/TejasKamat/vendor_xiaomi_mojito $vt
+		git clone https://github.com/TejasKamat/kernel_xiaomi_mojito $kt
+	else
+		git clone https://github.com/TejasKamat/vendor_xiaomi_mojito --depth=1 $vt
+		git clone https://github.com/TejasKamat/kernel_xiaomi_mojito --depth=1 $kt
+	fi
+}
+
+# Some checks
+if [ ! -d $vt ]
+then
+	rm -rf hardware/xiaomi/megvii
+	clone
+fi
+
